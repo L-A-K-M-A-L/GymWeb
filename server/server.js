af0b74b00f8bf44a1b2878a9fe09899b5c1f3462
Lakmal Prasanna <lakmalprasannakd@gmail.com>
2024-12-02 00:20:56 +0530

    Update url

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+const timeout = require('connect-timeout');
 const express = require('express');
 const cors = require('cors');
 const connection = require('./config/db');
