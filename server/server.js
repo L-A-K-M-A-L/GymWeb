bdaf10f62b24242a7a7540e403938c4e7a66ecb0
Lakmal Prasanna <lakmalprasannakd@gmail.com>
2024-12-02 01:03:48 +0530

    Update url

--- server/server.js
+++ server/server.js
@@ -18,7 +18,7 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 // app.options('*', cors(corsOptions));
-app.use(timeout('120000'));
+// app.use(timeout('120000'));
 app.use(express.json());
 app.use('/', userRoutes);
 
